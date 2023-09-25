c1f65f4f88742983ceb22e2a895f282d8f1b891a
ymmot239 <pacman239sa@gmail.com>
2023-09-25 15:50:46 -0500

    refactoed and added firebase

--- lib/materials.dart
+++ lib/materials.dart
@@ -1,6 +1,8 @@
 //import 'dart:js_interop';
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+//import 'firebase_options.dart';
 
 import 'edit.dart';
 import 'logo_icon.dart';
@@ -13,19 +15,23 @@ class Materials extends StatefulWidget {
 }
 
 class _MaterialsState extends State<Materials> {
-  final List<dynamic> listSelection = [
-    {
-      'id': 0,
-      'header': 'Tap to add customer',
-      'icon': const Icon(Icons.add_circle_outlined),
-      //'taps': _addNewUploadDoc(),
-    },
-  ];
-
-  //int _selected = 0;
+  List<dynamic> listSelection = [];
   int index = 0;
   int _count = 1;
-  //late List<Widget> _upload = List.generate(_count, (int i) => Materials());
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    listSelection = [
+      {
+        'id': 0,
+        'header': 'Tap to add customer',
+        'icon': const Icon(Icons.add_circle_outlined),
+        //'taps': _addNewUploadDoc(),
+      },
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,33 +48,36 @@ class _MaterialsState extends State<Materials> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Expanded(
-                  child: ListView.separated(
-                itemCount: _count,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      ListTile(
-                        title: Text(listSelection[index]['header']),
-                        onTap: () {
-                          if (index != _count - 1) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    Edit(customdata: listSelection[index]),
-                              ),
-                            );
-                          } else {
-                            _addNewCustomer();
-                          }
-                        },
-                        leading: listSelection[index]['icon'],
-                      ),
-                    ],
-                  );
-                },
-                separatorBuilder: (context, index) =>
-                    const Divider(thickness: 1),
+                  child: Scrollbar(
+                child: ListView.separated(
+                  controller: scrollController,
+                  itemCount: _count,
+                  separatorBuilder: (context, index) =>
+                      const Divider(thickness: 1),
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        ListTile(
+                          title: Text(listSelection[index]['header']),
+                          onTap: () {
+                            if (index != _count - 1) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      Edit(customdata: listSelection[index]),
+                                ),
+                              );
+                            } else {
+                              _addNewCustomer();
+                            }
+                          },
+                          leading: listSelection[index]['icon'],
+                        ),
+                      ],
+                    );
+                  },
+                ),
               ))
             ],
           ),
@@ -86,5 +95,11 @@ class _MaterialsState extends State<Materials> {
       });
       _count++;
     });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
   }
 }
