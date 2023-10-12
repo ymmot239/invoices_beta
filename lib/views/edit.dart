5af991f3dbe2179f5b533193f49922a2859b6030
ymmot239 <pacman239sa@gmail.com>
2023-10-12 14:15:13 -0500

    made working lists for house and customers

--- lib/views/edit.dart
+++ lib/views/edit.dart
@@ -55,104 +55,181 @@ class _EditState extends State<Edit> {
               mainAxisAlignment: MainAxisAlignment.start,
               children:
                   List<Widget>.generate(Customer.fields.length, (int index) {
-                if (index == Customer.fields.length - 1) {}
-                return Flexible(
+                if (index == Customer.fields.length - 1) {
+                  return Flexible(
                     flex: 5,
                     child: Container(
-                      decoration: BoxDecoration(
-                          //borderRadius: BorderRadius.circular(10),
-                          //color: Colors.red,
-                          border: Border.all(
-                        color:
-                            Colors.black, //Theme.of(context).primaryColorDark,
-                        width: 2,
-                      )),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [],
-                      ),
-                    ));
-                // return Row(
-                //     mainAxisAlignment: MainAxisAlignment.center,
-                //     children: <Widget>[
-                //       Flexible(
-                //         flex: 1,
-                //         child: Container(
-                //             width: double.infinity,
-                //             alignment: Alignment.center,
-                //             height: (() {
-                //               if (index == Customer.fields.length - 1) {
-                //                 return constraints.maxHeight * .6;
-                //               }
-                //               return constraints.maxHeight * .1;
-                //             })(),
-                //             decoration: BoxDecoration(
-                //                 //color: Theme.of(context).primaryColorDark,
-                //                 border: Border.all(
-                //               color: Colors
-                //                   .black, //Theme.of(context).primaryColorLight,
-                //               width: 2,
-                //             )),
-                //             padding: EdgeInsets.all(8.0), //BoxDecoration
-                //             child: Text(Customer.fields[index],
-                //                 style: Theme.of(context)
-                //                     .textTheme
-                //                     .bodyMedium
-                //                     ?.copyWith(
-                //                         color: Colors
-                //                             .black) //Theme.of(context).primaryColor),
-                //                 )),
-                //       ),
-                //       Flexible(
-                //         flex: 2,
-                //         child: Container(
-                //           width: double.infinity,
-                //           //height: double.infinity,
-                //           height: (() {
-                //             if (index == Customer.fields.length - 1) {
-                //               return constraints.maxHeight * .6;
-                //             }
-                //             return constraints.maxHeight * .1;
-                //           })(),
-                //           decoration: BoxDecoration(
-                //               //borderRadius: BorderRadius.circular(10),
-                //               //color: Colors.red,
-                //               border: Border.all(
-                //             color: Colors
-                //                 .black, //Theme.of(context).primaryColorDark,
-                //             width: 2,
-                //           )),
-                //           padding: EdgeInsets.all(8.0),
-                //           child: (() {
-                //             if (index != Customer.fields.length - 1) {
-                //               return TextFormField(
-                //                 initialValue:
-                //                     customdata.toJson()[Customer.fields[index]],
-                //                 textAlign: TextAlign.center,
-                //                 style: Theme.of(context)
-                //                     .textTheme
-                //                     .bodyMedium
-                //                     ?.copyWith(
-                //                         color: Colors
-                //                             .black), //Theme.of(context).primaryColor),
-                //                 onFieldSubmitted: (String value) {
-                //                   Map<String, dynamic> temp =
-                //                       customdata.toJson();
-                //                   temp[Customer.fields[index]] = value;
-                //                   customerNotifier.value[widget.index] =
-                //                       Customer.fromJson(temp);
-                //                   setState(() {});
-                //                 },
-                //               );
-                //             }
-                //             return ListAdditions(
-                //                 list: customdata.Houses,
-                //                 newItem: newItem,
-                //                 editItem: editItem);
-                //           })(),
-                //         ),
-                //       ),
-                //     ]);
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2),
+                        ),
+                        child: Row(children: [
+                          Flexible(
+                            flex: 1,
+                            child: Container(
+                                width: double.infinity,
+                                child: Text(Customer.fields[index],
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium
+                                        ?.copyWith(
+                                            color: Colors
+                                                .black) //Theme.of(context).primaryColor),
+                                    )),
+                          ),
+                          Flexible(
+                              flex: 2,
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                    border: Border(
+                                        left: BorderSide(
+                                            color: Colors.black, width: 2))),
+                                child: ListAdditions(
+                                    list: customdata.Houses,
+                                    newItem: newItem,
+                                    editItem: editItem),
+                              ))
+                        ])),
+                  );
+                } else {
+                  return Flexible(
+                      flex: 1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            //borderRadius: BorderRadius.circular(10),
+                            //color: Colors.red,
+                            border: Border.all(
+                          color: Colors
+                              .black, //Theme.of(context).primaryColorDark,
+                          width: 2,
+                        )),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Flexible(
+                                flex: 1,
+                                child: Container(
+                                  width: double.infinity,
+                                  child: Text(Customer.fields[index],
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium
+                                          ?.copyWith(
+                                              color: Colors
+                                                  .black) //Theme.of(context).primaryColor),
+                                      ),
+                                )),
+                            Flexible(
+                                flex: 2,
+                                child: Container(
+                                    decoration: const BoxDecoration(
+                                        border: Border(
+                                            left: BorderSide(
+                                                color: Colors.black,
+                                                width: 2))),
+                                    child: TextFormField(
+                                      initialValue: customdata
+                                          .toJson()[Customer.fields[index]],
+                                      textAlign: TextAlign.center,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium
+                                          ?.copyWith(
+                                              color: Colors
+                                                  .black), //Theme.of(context).primaryColor),
+                                      onFieldSubmitted: (String value) {
+                                        Map<String, dynamic> temp =
+                                            customdata.toJson();
+                                        temp[Customer.fields[index]] = value;
+                                        customerNotifier.value[widget.index] =
+                                            Customer.fromJson(temp);
+                                        setState(() {});
+                                      },
+                                    )))
+                          ],
+                        ),
+                      ));
+                  // return Row(
+                  //     mainAxisAlignment: MainAxisAlignment.center,
+                  //     children: <Widget>[
+                  //       Flexible(
+                  //         flex: 1,
+                  //         child: Container(
+                  //             width: double.infinity,
+                  //             alignment: Alignment.center,
+                  //             height: (() {
+                  //               if (index == Customer.fields.length - 1) {
+                  //                 return constraints.maxHeight * .6;
+                  //               }
+                  //               return constraints.maxHeight * .1;
+                  //             })(),
+                  //             decoration: BoxDecoration(
+                  //                 //color: Theme.of(context).primaryColorDark,
+                  //                 border: Border.all(
+                  //               color: Colors
+                  //                   .black, //Theme.of(context).primaryColorLight,
+                  //               width: 2,
+                  //             )),
+                  //             padding: EdgeInsets.all(8.0), //BoxDecoration
+                  //             child: Text(Customer.fields[index],
+                  //                 style: Theme.of(context)
+                  //                     .textTheme
+                  //                     .bodyMedium
+                  //                     ?.copyWith(
+                  //                         color: Colors
+                  //                             .black) //Theme.of(context).primaryColor),
+                  //                 )),
+                  //       ),
+                  //       Flexible(
+                  //         flex: 2,
+                  //         child: Container(
+                  //           width: double.infinity,
+                  //           //height: double.infinity,
+                  //           height: (() {
+                  //             if (index == Customer.fields.length - 1) {
+                  //               return constraints.maxHeight * .6;
+                  //             }
+                  //             return constraints.maxHeight * .1;
+                  //           })(),
+                  //           decoration: BoxDecoration(
+                  //               //borderRadius: BorderRadius.circular(10),
+                  //               //color: Colors.red,
+                  //               border: Border.all(
+                  //             color: Colors
+                  //                 .black, //Theme.of(context).primaryColorDark,
+                  //             width: 2,
+                  //           )),
+                  //           padding: EdgeInsets.all(8.0),
+                  //           child: (() {
+                  //             if (index != Customer.fields.length - 1) {
+                  //               return TextFormField(
+                  //                 initialValue:
+                  //                     customdata.toJson()[Customer.fields[index]],
+                  //                 textAlign: TextAlign.center,
+                  //                 style: Theme.of(context)
+                  //                     .textTheme
+                  //                     .bodyMedium
+                  //                     ?.copyWith(
+                  //                         color: Colors
+                  //                             .black), //Theme.of(context).primaryColor),
+                  //                 onFieldSubmitted: (String value) {
+                  //                   Map<String, dynamic> temp =
+                  //                       customdata.toJson();
+                  //                   temp[Customer.fields[index]] = value;
+                  //                   customerNotifier.value[widget.index] =
+                  //                       Customer.fromJson(temp);
+                  //                   setState(() {});
+                  //                 },
+                  //               );
+                  //             }
+                  //             return ListAdditions(
+                  //                 list: customdata.Houses,
+                  //                 newItem: newItem,
+                  //                 editItem: editItem);
+                  //           })(),
+                  //         ),
+                  //       ),
+                  //     ]);
+                }
               }),
             ),
           );
